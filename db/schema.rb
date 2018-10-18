4ba83b2268524cc61731f2e3a2f785808b18656c
mgonline <mgonline@mgonlines-MacBook-Pro.local>
2018-10-18 22:05:19 +0200

    create Contact model and add reference to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_18_191806) do
+ActiveRecord::Schema.define(version: 2018_10_18_200410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.text "address"
+    t.string "city"
+    t.string "zip_code"
+    t.string "country"
+    t.string "phone_number"
+    t.string "email"
+    t.string "home_page"
+    t.text "company_description"
+    t.integer "year_of_establishment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,8 +38,11 @@ ActiveRecord::Schema.define(version: 2018_10_18_191806) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "user_name"
+    t.bigint "contact_id"
+    t.index ["contact_id"], name: "index_users_on_contact_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "users", "contacts"
 end
