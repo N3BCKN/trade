cbc32d8b7bce32ab3f8641193fd1c75ed0d687c6
mgonline <mgonline@mgonlines-MacBook-Pro.local>
2018-11-06 18:06:11 +0100

    configure email notifications

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,6 @@
 class MessagesController < ApplicationController
+
+
   def index
   end
 
@@ -6,5 +8,37 @@ class MessagesController < ApplicationController
   end
 
   def create
+  	@message         = Message.create(message_params)
+  	@message.user_id = current_user.id
+  	@message.lead_id = params[:id]
+  	@lead            = Lead.find(params[:id])
+  	
+
+  	NotificationsMailer.send_lead_response(
+  	@message,
+  	@lead, 
+  	params[:absolute_url]
+  	).deliver_now
+
+  	respond_to do |format|
+	  	if @message.save
+	  		format.html { redirect_to root_path, 
+	  			notice:  "Your message has been saved"}
+	  	else
+	  		format.html { render :new,
+	  			notice: "Message couldn't be delivered"}
+	  	end
+    end
+  end
+
+  private
+  def message_params
+  	params.require(:message).permit(
+  	 :content,
+  	 :email,
+  	 :phone_number,
+  	 :country,
+  	 :contact_person
+  	)
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,9 @@
 class Message < ApplicationRecord
   belongs_to :lead
   belongs_to :user
+
+  validates :country, length: {minimum: 2, maximum: 80}, allow_blank: true
+  validates :phone_number, length: {minimum: 7, maximum: 30}, allow_blank: true
+  validates :content, presence: true, length: {minimum: 10, maximum: 1500}
+  validates :contact_person, length: {minimum: 5, maximum: 120}
 end
