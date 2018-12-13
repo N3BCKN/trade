ef9be4b851dde981d4fa8a23987b8690374b8ec9
mgonline <mgonline@mgonlines-MacBook-Pro.local>
2018-12-13 12:12:20 +0100

    email production next approach

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { 
+    :host => "https://shrouded-everglades-50569.herokuapp.com/"}
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
