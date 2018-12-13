3ae36ec6c2d876aefec95ce321ab29a9ef7a1f7c
maurycy <maurycy@users.noreply.github.com>
2018-12-13 13:08:45 +0100

    facepalm

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :user_name, presence: true, length: {minimum: 4, maximum: 25}
 
-  validates :email, presence: true, unique: true
+  validates :email, presence: true, uniqueness: true
   validates :encrypted_password, presence: true
 
 
