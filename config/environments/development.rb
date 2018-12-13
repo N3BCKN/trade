f011be25495232e9c68a91a60166bb751998d785
mgonline <mgonline@mgonlines-MacBook-Pro.local>
2018-12-13 08:10:22 +0100

    production mailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
 
 
   config.action_mailer.delivery_method = :letter_opener
-  # config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
