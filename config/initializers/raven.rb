ec7ea7f5ac34dd348aca4d514e573cfce652426b
maurycy <maurycy@users.noreply.github.com>
2018-12-13 13:14:19 +0100

    do not config sentry on non-prod envs

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,3 +1,5 @@
-Raven.configure do |config|
-  config.dsn = 'https://119e4642e1a9477ab414da381e428013:40df6f801c5d46ec8001872d3c07937c@sentry.io/1352862'
+if Rails.env.production?
+  Raven.configure do |config|
+    config.dsn = 'https://119e4642e1a9477ab414da381e428013:40df6f801c5d46ec8001872d3c07937c@sentry.io/1352862'
+  end
 end
