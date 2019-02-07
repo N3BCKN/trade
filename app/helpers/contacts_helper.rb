07d0316ea2d756705d443cb6871214e9a862f61e
H3BCKN <mgonline@mgonlines-MacBook-Pro.local>
2019-02-07 08:30:46 +0100

    navbar avarat tiny

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -8,4 +8,12 @@ module ContactsHelper
       image_tag contact.avatar.url, class: 'img-fluid', id: img_id
     end
   end
+
+  def set_avatar_tiny(user)
+  	if user.contact.avatar_file_name.nil?
+  		'assets/avatar_default_tiny.png'
+  	else
+  		contact.avatar.url(:tiny)
+  	end
+  end 
 end
