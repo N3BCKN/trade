7a11e4e716e57147bf26a5537216b490b8b25fdd
H3BCKN <mgonline@mgonlines-MacBook-Pro.local>
2019-03-18 05:51:08 +0100

    add db slug for leads

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_17_051619) do
+ActiveRecord::Schema.define(version: 2019_03_18_044724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,17 @@ ActiveRecord::Schema.define(version: 2019_03_17_051619) do
     t.index ["user_id"], name: "index_favorite_leads_on_user_id"
   end
 
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
+  end
+
   create_table "leads", force: :cascade do |t|
     t.string "title"
     t.string "lead_status"
@@ -75,8 +86,10 @@ ActiveRecord::Schema.define(version: 2019_03_17_051619) do
     t.datetime "product_image_updated_at"
     t.text "description_short"
     t.bigint "category_id"
+    t.string "slug"
     t.index ["category_id"], name: "index_leads_on_category_id"
     t.index ["deleted_at"], name: "index_leads_on_deleted_at"
+    t.index ["slug"], name: "index_leads_on_slug", unique: true
     t.index ["user_id"], name: "index_leads_on_user_id"
   end
 
