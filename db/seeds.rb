d51ecb49443c64c452ea34bcff46a2d2853c317c
H3BCKN <mgonline@mgonlines-MacBook-Pro.local>
2019-04-07 05:17:43 +0200

    new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,12 @@ fugit debitis repellat praesentium cupiditate at! Sequi.Lorem ipsum dolor sit am
  ipsa nam sunt, iusto! Natus, consequatur! Accusamus, perferendis?Lorem ipsum dolor sit amet, 
  consectetur adipisicing elit."
 
+ areas = ["Cosmetics / Body Care", "Agriculture", "Construction Materials", "Office/Packaging Products"]
+
+ freqency = ["Single Delivery", "Once a week", "Once a Month", "Once a Year", "Several Times Per Year", "Another Way"]
+
+ continents = ["Africa", "Asia", "Europe", "South America", "North America", "Australia"]
+
 400.times do |t|
 password = Faker::Internet.password(64)
 
@@ -37,6 +43,9 @@ Contact.create!(
 	country: Faker::Address.country_code,
 	company_description: lorem,
 	phone_number: Faker::PhoneNumber.phone_number,
+	number_of_employes: rand(500),
+	areas_of_interest: areas,
+	annual_sales: "5000000$"
 	user_id: t+1
 )
 puts "User number #{t} created"
@@ -64,22 +73,26 @@ Category.create!(name: "Office/Packaging Products")
   @user  = User.find(rand(1..User.count))
 
 Lead.create!(
-	title:            Faker::GreekPhilosophers.quote,
-	description:      lorem,
-	description_short:lorem.slice(0..300),
-	lead_status:      rand(2) == 1 ? "offer" : "product",
-	contact_person:   @user.user_name,
-	address:          Faker::Address.street_address,
-	city:             Faker::Address.city,
-	zip_code:         Faker::Address.zip_code,
-	country:          Faker::Address.country_code,
-	home_page:        Faker::Internet.url('trade.com'),
-	phone_number:     Faker::PhoneNumber.phone_number,
-	email:            @user.email,
-	created_at:    	  @dateOfCreation,
-	updated_at:    	  @dateOfCreation,
-	category_id:      Category.find(rand(1..Category.count)).id,
-	user_id:          @user.id
+	title:              Faker::GreekPhilosophers.quote,
+	description:        lorem,
+	description_short:  lorem.slice(0..300),
+	lead_status:        rand(2) == 1 ? "offer" : "product",
+	contact_person:     @user.user_name,
+	address:            Faker::Address.street_address,
+	city:               Faker::Address.city,
+	zip_code:           Faker::Address.zip_code,
+	country:            Faker::Address.country_code,
+	home_page:          Faker::Internet.url('trade.com'),
+	phone_number:       Faker::PhoneNumber.phone_number,
+	destination:        Faker::University.name,
+	quantity:           "50 tonnes",
+	freqency:           freqency.sample,
+	prefered_suppliers: continents.sample,
+	email:              @user.email,
+	created_at:    	    @dateOfCreation,
+	updated_at:    	    @dateOfCreation,
+	category_id:        Category.find(rand(1..Category.count)).id,
+	user_id:            @user.id
 )
 puts "Lead number #{p} created"
 end 
