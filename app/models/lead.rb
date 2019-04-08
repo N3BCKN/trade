d2414887db8f85023b60b52630f79cb47819ade4
H3BCKN <mgonline@mgonlines-MacBook-Pro.local>
2019-04-08 15:56:18 +0200

    fix lead model validation for non-require fields

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -20,18 +20,18 @@ class Lead < ApplicationRecord
   }
 
   validates :title, presence: true, length: { minimum: 5, maximum: 80 }
-  validates :description, presence: true, length: { minimum: 5, maximum: 1800 }
+  validates :description, presence: true, length: { minimum: 8, maximum: 1800 }
   validates :lead_status, presence: true, length: { minimum: 5, maximum: 7 }
-  validates :contact_person, length: { minimum: 5, maximum: 120 }
-  validates :address, length: { minimum: 5, maximum: 120 }
-  validates :city, length: { minimum: 3, maximum: 80 }
-  validates :zip_code, length: { minimum: 4, maximum: 25 }
+  validates :contact_person, length: { minimum: 2, maximum: 120 }, allow_blank: true
+  validates :address, length: { minimum: 5, maximum: 120 }, allow_blank: true
+  validates :city, length: { minimum: 3, maximum: 80 }, allow_blank: true
+  validates :zip_code, length: { minimum: 4, maximum: 25 }, allow_blank: true
   validates :country, length: { minimum: 2, maximum: 8 }
-  validates :phone_number, length: { minimum: 7, maximum: 30 }
-  validates :destination, length: { minimum: 2, maximum: 85 }
-  validates :quantity, length: { minimum: 2, maximum: 70 }
-  validates :freqency, length: { minimum: 5, maximum: 45 }
-  validates :prefered_suppliers, length: { minimum: 4, maximum: 55 }
+  validates :phone_number, length: { minimum: 6, maximum: 30 }, allow_blank: true
+  validates :destination, length: { minimum: 2, maximum: 85 }, allow_blank: true
+  validates :quantity, length: { minimum: 2, maximum: 70 }, allow_blank: true
+  validates :freqency, length: { minimum: 9, maximum: 24 }
+  validates :prefered_suppliers, length: { minimum: 2, maximum: 55 }, allow_blank: true
 
   has_attached_file :product_image, styles:      { default: '650x400>', thumb: '150x150>' },
                                     default_url: '/images/:style/default_product_image.png'
