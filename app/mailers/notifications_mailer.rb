b0884ed6597133aa3ce119e9e48b017aa2163ffe
H3BCKN <mgonline@mgonlines-MacBook-Pro.local>
2019-07-09 23:59:39 +0200

    change logo on email tamplates

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -2,6 +2,7 @@
 
 class NotificationsMailer < ApplicationMailer
   def send_lead_response(message, lead, url_path)
+  	attachments.inline["logo.png"] = File.read("#{Rails.root}/app/assets/images/logo.png")
     @message = message
     @lead    = lead
     @url     = url_path
