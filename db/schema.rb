295d9d1058a34f00e94efa65076e76a17f47b096
H3BCKN <mgonline@mgonlines-MacBook-Pro.local>
2019-07-12 22:59:43 +0200

    remove email_confirmed from user field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_12_125141) do
+ActiveRecord::Schema.define(version: 2019_07_12_173455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -159,7 +159,6 @@ ActiveRecord::Schema.define(version: 2019_07_12_125141) do
     t.datetime "updated_at", null: false
     t.string "user_name"
     t.datetime "deleted_at"
-    t.boolean "email_confirmed", default: false
     t.string "confirm_token"
     t.integer "role"
     t.boolean "terms_of_use", default: false
