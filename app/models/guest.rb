94c77c3df10cdd8118a59d2b9425cddb7cf51f60
H3BCKN <mgonline@mgonlines-MacBook-Pro.local>
2019-07-26 01:54:25 +0200

    rspec guest tests

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -3,5 +3,5 @@
 class Guest < ApplicationRecord
   has_many :views, dependent: :destroy
 
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -3,5 +3,31 @@
 require 'rails_helper'
 
 RSpec.describe Guest, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	subject { described_class.new(email: "email@email.com")}
+  describe 'Validations' do
+  	it 'should be valid with valid params' do
+  		expect(subject).to be_valid
+  	end 
+  	it 'should be saved' do
+  		expect(subject.save).to eq(true)
+  	end 
+  	describe 'email' do
+  		it 'should be present' do
+  			subject.email = nil
+  			expect(subject).to_not be_valid
+  		end
+  		it 'should be an email' do
+  			subject.email = "not_an_email"
+  			expect(subject).to_not be_valid
+  			subject.email = "email.com"
+  			expect(subject).to_not be_valid
+  		end 
+  	end 
+  end
+  describe 'Assosiations' do
+  	it 'must belongs to the user' do
+      assc = described_class.reflect_on_association(:views)
+      expect(assc.macro).to eq(:has_many)
+    end
+  end
 end
