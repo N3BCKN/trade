01a5b59cf88422c059500ba70f4f0ce12e5fa6a8
H3BCKN <mgonline@mgonlines-MacBook-Pro.local>
2019-07-26 01:41:17 +0200

    rspec messages tests

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,6 +7,7 @@ class Message < ApplicationRecord
 
   validates :country, length: { minimum: 2, maximum: 80 }, allow_blank: true
   validates :phone_number, length: { minimum: 7, maximum: 30 }, allow_blank: true
-  validates :content, presence: true, length: { minimum: 10, maximum: 1500 }
+  validates :content, presence: true, length: { minimum: 8, maximum: 1500 }
   validates :contact_person, length: { minimum: 5, maximum: 120 }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
