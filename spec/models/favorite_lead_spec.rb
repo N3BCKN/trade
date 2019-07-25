d7709657481e75e4a15fd1e5bb801b8c7fda4be7
H3BCKN <mgonline@mgonlines-MacBook-Pro.local>
2019-07-26 01:15:06 +0200

    favorite leads rspec tests

--- spec/models/favorite_lead_spec.rb
+++ spec/models/favorite_lead_spec.rb
@@ -3,5 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe FavoriteLead, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Assosiations" do
+  	it 'must belongs to user' do
+  		assc = described_class.reflect_on_association(:user)
+    	expect(assc.macro).to eq(:belongs_to)
+  	end 
+  	it 'must belongs to lead' do
+  		assc = described_class.reflect_on_association(:lead)
+    	expect(assc.macro).to eq(:belongs_to)
+  	end 
+  end 
 end
