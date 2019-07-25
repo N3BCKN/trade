6c57a23aa1da060d189e5ae15cda9bf32fe1e7f6
H3BCKN <mgonline@mgonlines-MacBook-Pro.local>
2019-07-25 21:15:51 +0200

    add Abuse model rspec test

--- app/models/abuse.rb
+++ app/models/abuse.rb
@@ -4,6 +4,6 @@ class Abuse < ApplicationRecord
   belongs_to :lead
   belongs_to :user
 
-  validates :content, length: { minimum: 4, maximum: 850 }, presence: true
-  validates :link, presence: true
+  validates :content, length: { minimum: 5, maximum: 850 }, presence: true
+  validates :link, presence: true, length: { minimum: 8 }
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,13 +15,14 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require 'elasticsearch/extensions/test/cluster'
+# require 'elasticsearch/extensions/test/cluster'
 require 'yaml'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
+  config.formatter = :documentation
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
